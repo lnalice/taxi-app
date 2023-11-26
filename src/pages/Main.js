e9da1e0806a880d089679849a8ccfbad5b0a73d4
lnalice <rara9915@gmail.com>
2023-11-26 17:48:25 +0900

    Add about 150 taxis

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,8 @@
-import React, { useEffect, useState, useRef} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+
+//------------------------BUSSAMPLE-----------------------------
+const driver = require('../data/driver.json');
+//--------------------------------------------------------------
 
 const { kakao } = window;
 
@@ -7,37 +11,12 @@ export default function Main() {
 	const [map, setMap] = useState(null);
 
 	var index = 0;
-	var customOverlayMarker;
-	var prevDegree;
-
-	const seoulLocation = [
-		[37.47955935212742, 126.94312011533331],
-		[37.47957794314708, 126.94431852476892],
-		[37.4795004314708, 126.94500852476892],
-		[37.47941636191352, 126.94560751248083],
-		[37.47930006191352, 126.94600751248083],
-		[37.47929101942485, 126.94737131425384],
-		[37.47918344574943, 126.94861503023887],
-		[37.47905783879834, 126.94985875471353],
-		[37.47891412357071, 126.95092159430716],
-		[37.478824505208976, 126.95209745291311],
-		[37.47922106798528, 126.95239115132607],
-		[37.48068981242196, 126.95263895259016],
-		[37.481347302479165, 126.95202800554102],
-		[37.48152707261361, 126.95097641620833],
-		[37.48164376757, 126.94993617012781],
-		[37.48177844906954, 126.9488393774952],
-		[37.48188603004106, 126.94760692504032],
-		[37.48196653708894, 126.9463066509525],
-		[37.48209213806814, 126.94513070990521],
-		[37.48213213806814, 126.94413070990521],
-		[37.482235768949664, 126.94399997659112],
-		[37.48235238061101, 126.94291448472713],
-	]
+	var markers = []
+	var kakaoMap
 
 	const headingDeg = async function ([beforeX, beforeY], [nextX, nextY]) {
 		var rad = Math.atan2(nextY - beforeY, nextX - beforeX);
-		return (rad*180)/Math.PI ;
+		return (rad * 180) / Math.PI;
 	}
 
 	useEffect(() => {
@@ -45,27 +24,64 @@ export default function Main() {
 		const container = document.getElementById('map');
 		const options = {
 			center: new kakao.maps.LatLng(process.env.REACT_APP_MAP_LATITUDE,
-				process.env.REACT_APP_MAP_LOGITUDE)
+				process.env.REACT_APP_MAP_LOGITUDE),
+			
 		};
-		const kakaoMap = new kakao.maps.Map(container, options);
+		kakaoMap = new kakao.maps.Map(container, options);
+		kakaoMap.setLevel(5);
 
 		//줌 컨트롤
 		var zoomControl = new kakao.maps.ZoomControl();
 		kakaoMap.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 		setMap(kakaoMap);
-		
-		//커스텀 오버레이 이동
-		setInterval(async function interval () {
-			if (customOverlayMarker) customOverlayMarker.setMap(null);
-			
-			if (index < seoulLocation.length) {
-				let [lat, lon] = seoulLocation[index];
-				// console.log(lat, lon, "lan / lon");
 
-				var deg = await headingDeg((index>0)? seoulLocation[index-1] : seoulLocation[index], seoulLocation[index])-90;
+		//마커 배열
+		for (let i = 0; i< driver.length; i++){
+			addOverlay(i, kakaoMap);
+		}
+		setMarkers(kakaoMap);
+
+	}, []);
+
+	async function addOverlay(i, kakaoMap) {
+		var deg = await headingDeg(
+			(i > 0) ?
+				[driver[i-1].lat, driver[i-1].lng]
+				: [driver[i].lat, driver[i].lng],
+			[driver[i].lat, driver[i].lng]) - 90;
+		// console.log(deg, "heading");
+		// console.log(driver[i].lat, driver[i].lng, "driver[i].lat, driver[i].lng");
+
+		var marker = new kakao.maps.CustomOverlay({
+			map: kakaoMap,
+			position: new kakao.maps.LatLng(driver[i].lat, driver[i].lng),
+			content: `<img src="/taxi_upper_side.png" style="width:34px;height:17px;transform:rotate(${deg}deg);">`
+		})
+		marker.setMap(kakaoMap);
+		markers.push(marker);
+	}
+
+	function setMarkers(kakaoMap) {
+		for (var i = 0; i < markers.length; i++) {
+			markers[i].setMap(kakaoMap);
+		}
+	}
+
+	function moveOverlay(driver,kakaoMap, customOverlayMarker) {
+
+		//커스텀 오버레이 이동
+		setInterval(async function interval() {
+			if (customOverlayMarker) customOverlayMarker.setMap(null);
+
+			if (i < driver.length) {
+				var deg = await headingDeg(
+					(i > 0) ?
+						[driver[i-1].lat, driver[i-1].lng]
+						: [driver[i].lat, driver[i].lng],
+					[driver[i].lat, driver[i].lng]) - 90;
 				console.log(deg, "heading");
 
-				if (index > 0) {
+				if (i > 0) {
 					customOverlayMarker = new kakao.maps.CustomOverlay({
 						position: new kakao.maps.LatLng(process.env.REACT_APP_MAP_LATITUDE, process.env.REACT_APP_MAP_LOGITUDE),
 						content: `<img src="/taxi_upper_side.png" style="width:40px;height:20px;transform:rotate(${deg}deg);">`
@@ -76,16 +92,13 @@ export default function Main() {
 					await customOverlayMarker.setPosition(new kakao.maps.LatLng(lat, lon));
 				}
 
-				index++;
-				// console.log(index, "index");
-
+				i++;
 			} else {
-				clearInterval(interval); // 배열의 끝에 도달하면 타이머 종료
+				clearInterval(interval); 
 			}
-	
-		}, 5000);
-	}, []);
 
+		}, 5000);
+	}
 
 	return (
 		<div id="map" style={{
